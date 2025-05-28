7e1d022b83b3f2ef9ec40c567c4e4f1fca1afcfc
kamScripts <k.gebski1@gmail.com>
2025-05-28 19:23:42 +0100

    fetch

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+const stats = document.querySelectorAll('td')
 
-
+const fetchAllCreatures = async ()=>{
+    url='https://rpg-creature-api.freecodecamp.rocks/api/creatures'
+    fetch(url, {mode: "cors"}).then((response)=>{
+        return response.json()
+    }).then((response)=>{
+        console.log(response)
+    });
+}
 const fetchData = async (id) => {
     url=`https://rpg-creature-api.freecodecamp.rocks/api/creature/${id}`
     try {
         const res = await fetch(url)
         const data = await res.json()
         console.log(data)
+        showDetails(data)
+        
     } catch (err) {
         console.log(err)
+        alert("Creature not found")
     }
 }
-fetchData(1)
+const showDetails = (data)=>{
+    for (let i = 0; i < stats.length; i++) {
+            stats[i].textContent=data.stats[i].base_stat
+    }
+}
+fetchAllCreatures()
+fetchData('1')
