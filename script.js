e879d32c46fd2ac6c17ace66d5a958d84c310104
kamScripts <k.gebski1@gmail.com>
2025-05-28 18:49:38 +0100

    add async

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello')
+
+
+const fetchData = async (id) => {
+    url=`https://rpg-creature-api.freecodecamp.rocks/api/creature/${id}`
+    try {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+}
+fetchData(1)
