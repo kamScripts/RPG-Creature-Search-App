4cdcc336be19d21ed79ce1eb54f3dfbfce8042b2
kamScripts <k.gebski1@gmail.com>
2025-06-01 17:03:54 +0100

    add text content to elements

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const stats = document.querySelectorAll('td')
+const stats = document.querySelectorAll('td');
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search-input');
+const creatureName = document.querySelector('#creature-name');
+const id = document.querySelector('#creature-id');
+const weight = document.querySelector('#weight');
+const height = document.querySelector('#height');
+const types = document.querySelector('#types');
+const output = document.querySelector('#top');
 
+// Fetch names of all creatures
 const fetchAllCreatures = async ()=>{
-    url='https://rpg-creature-api.freecodecamp.rocks/api/creatures'
+    const url='https://rpg-creature-api.freecodecamp.rocks/api/creatures'
     fetch(url, {mode: "cors"}).then((response)=>{
         return response.json()
     }).then((response)=>{
         console.log(response)
     });
-}
+};
 const fetchData = async (id) => {
-    url=`https://rpg-creature-api.freecodecamp.rocks/api/creature/${id}`
+    const url=`https://rpg-creature-api.freecodecamp.rocks/api/creature/${id}`
     try {
         const res = await fetch(url)
         const data = await res.json()
@@ -20,11 +29,38 @@ const fetchData = async (id) => {
         console.log(err)
         alert("Creature not found")
     }
-}
+};
 const showDetails = (data)=>{
+    id.textContent ='#'+ data.id;
+    creatureName.textContent = data.name;
+    weight.textContent = 'Weight: '+data.weight;
+    height.textContent = 'Height: '+data.height;
+
+    
     for (let i = 0; i < stats.length; i++) {
             stats[i].textContent=data.stats[i].base_stat
-    }
-}
-fetchAllCreatures()
-fetchData('1')
+    };
+    for (let i=0; i <data.types.length;i++) {
+        let span = document.createElement('span');
+        span.classList.add('type');
+        let typeName = data.types[i].name.toUpperCase()
+        span.textContent =typeName;
+        types.appendChild(span)
+    };
+    let container = document.createElement('div');
+    let specialName = document.createElement('h3');
+    let description = document.createElement('p');
+    specialName.textContent = data.special.name;
+    description.textContent = data.special.description;
+    container.append(specialName, description)
+    output.appendChild(container)
+};
+
+form.addEventListener('submit', (e)=>{
+types.innerHTML='';
+e.preventDefault()
+let val = searchInput.value
+fetchData(val)
+form.reset()
+});
+
